fe4dd4daa346e7a54a16a5e3be2b6e19e576ea99
Ilya Velikiy <Ilya.Velikiy.java@yandex.by>
2022-01-08 19:12:18 +0300

    4 - Spring-MVC beginning

---
+++ web/src/main/java/by/academy/jee/web/servlet/common/LoginController.java
@@ -0,0 +1,57 @@
+package by.academy.jee.web.servlet.common;
+
+import by.academy.jee.exception.ServiceException;
+import by.academy.jee.model.person.Person;
+import by.academy.jee.web.service.Service;
+import javax.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import static by.academy.jee.web.constant.Constant.ALREADY_LOGGED_IN_JSP_URL;
+import static by.academy.jee.web.constant.Constant.ERROR_MESSAGE;
+import static by.academy.jee.web.constant.Constant.LOGIN_JSP_URL;
+import static by.academy.jee.web.constant.Constant.PASSWORD;
+import static by.academy.jee.web.constant.Constant.USER;
+import static by.academy.jee.web.constant.Constant.USER_NAME;
+
+@Slf4j
+@Controller
+@RequestMapping(value = {"/", "/login"})
+@RequiredArgsConstructor
+public class LoginController {
+
+    private final Service service;
+
+    @GetMapping
+    public String redirectBeforeLoginPage(HttpSession httpSession) {
+        if (httpSession.getAttribute(USER) != null) {
+            return ALREADY_LOGGED_IN_JSP_URL;
+        }
+        return LOGIN_JSP_URL;
+    }
+
+    @PostMapping
+    public ModelAndView login(@RequestParam(USER_NAME) String userName, @RequestParam(PASSWORD) String password,
+                              HttpSession httpSession) {
+
+        ModelAndView modelAndView = new ModelAndView();
+        try {
+            Person user = service.getUserIfExist(userName);
+            service.checkPassword(password, user);
+            httpSession.setAttribute(USER, user);
+            String role = user.getRole().toString();
+            log.info("User {} is successfully authorised, role - {}", user.getLogin(), role);
+            String menuUrl = service.getMenuUrlAfterLogin(role);
+            modelAndView.setViewName(menuUrl);
+        } catch (ServiceException e) {
+            modelAndView.addObject(ERROR_MESSAGE, e.getMessage());
+            modelAndView.setViewName(LOGIN_JSP_URL);
+        }
+        return modelAndView;
+    }
+}
